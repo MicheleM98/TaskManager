d3d80818ea1bc0e4a8d53d385f4b0a9a1c9149bf
MicheleM98 <55845560+MicheleM98@users.noreply.github.com>
2023-07-31 12:24:48 +0200

    third commit

--- src/AddTaskForm.js
+++ src/AddTaskForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useStore } from './store';
-import { Layout, Button, Space } from 'antd';
+import { Button, Space, Typography } from 'antd';
 import { CheckOutlined } from '@ant-design/icons';
 
 const AddTaskForm = () => {
@@ -8,7 +8,7 @@ const AddTaskForm = () => {
   const [description, setDescription] = useState('');
   const addTask = useStore((state) => state.addTask);
   const [size] = useState('large');
-  const { Sider, Content } = Layout;
+  const { Title } = Typography;
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -17,21 +17,19 @@ const AddTaskForm = () => {
     setTitle('');
     setDescription('');
   };
+  const titleStyle = {
+    color: '#fff',
+    marginBottom: '0px',
+  }
   
   return (
     <form>
       <Space size={size} direction='vertical'>
-        <h2>Nuovo Task</h2>
-        <div>
-            <label>Titolo:</label>
-            <br />
-            <input type="text" placeholder='Inserisci un titolo' value={title} onChange={(e) => setTitle(e.target.value)} />
-        </div>
-        <div>
-            <label>Description:</label>
-            <br />
-            <input type="text" placeholder='Inserisci una descriscione' value={description} onChange={(e) => setDescription(e.target.value)} />
-        </div>
+        <Title level={3} style={titleStyle}>Nuovo Task</Title>
+        <Title level={4} style={titleStyle}>Titolo:</Title>
+        <input type="text" placeholder='Inserisci un titolo' value={title} onChange={(e) => setTitle(e.target.value)} />
+        <Title level={4} style={titleStyle}>Description:</Title>
+        <input type="text" placeholder='Inserisci una descriscione' value={description} onChange={(e) => setDescription(e.target.value)} />
         <Button onClick={handleSubmit}>Crea <CheckOutlined /></Button>
       </Space>
     </form>
--- src/App.js
+++ src/App.js
@@ -2,31 +2,48 @@ import React from 'react';
 import { useStore } from './store';
 import TaskList from './TaskList';
 import AddTaskForm from './AddTaskForm';
-import { Layout } from 'antd';
+import { Layout, Typography} from 'antd';
 
 const App = () => {
   const { tasks, addTask, deleteTask } = useStore();
-  const { Sider, Content } = Layout;
+  const { Header, Footer, Sider, Content } = Layout;
+  const { Title } = Typography;
+  const headerStyle = {
+    textAlign: 'center',
+    height: 100,
+    paddingInline: 100,
+    lineHeight: '30px',
+    backgroundColor: '#108ee9',
+  };
   const contentStyle = {
-    textAlign: 'left',
-    minHeight: 120,
-    lineHeight: '12px',
-    backgroundColor: '#fff',
+    textAlign: 'center',
+    minHeight: 150,
+    lineHeight: '10px',
+    backgroundColor: '#7dbcea',
   };
   const siderStyle = {
     textAlign: 'center',
     lineHeight: '120px',
     backgroundColor: '#fff',
   };
+  const footerStyle = {
+    textAlign: 'center',
+    color: '#fff',
+    backgroundColor: '#108ee9',
+  };
+  const titleStyle = {
+    color: '#fff',
+  }
   return (
     <div>
-      <Layout hasSider>
+      <Layout>
         <Sider style={siderStyle}></Sider>
-        <Content style={contentStyle}>
-          <h1>Task App</h1>
-          <AddTaskForm onAddTask={addTask} />
-          <TaskList tasks={tasks} onDelete={deleteTask} />
-        </Content>
+        <Layout>
+          <Header style={headerStyle}><Title style={titleStyle}>Task App</Title></Header>
+          <Content style={contentStyle}><AddTaskForm onAddTask={addTask} /></Content>
+          <Footer style={footerStyle}><TaskList tasks={tasks} onDelete={deleteTask} /></Footer>
+        </Layout>
+        <Sider style={siderStyle}></Sider>
       </Layout>
     </div>
   );
